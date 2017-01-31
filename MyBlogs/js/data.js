87cb373bccbe3ef09a9eed5fd124ae7d51ab5983
iloveplus <1470886616@qq.com>
2017-01-31 18:35:22 +0800

    word批注

--- MyBlogs/js/data.js
+++ MyBlogs/js/data.js
@@ -25,6 +25,7 @@ var data=[
     {
         label:"个人作品",blogs:[
         { title:"js前端流程插件",url:"http://myflow.iloveplus.cn"},
+        { title:"word在线批注",url:"http://www.iloveplus.cn/highlighter/index.html"},
         { title:"react-native-calendar",url:"https://github.com/iloveplus/react-native-calendar/blob/master/README.md"},
         { title:"react-native-tooltip",url:"https://github.com/iloveplus/react-native-tooltip/blob/master/README.md"},
         ]
